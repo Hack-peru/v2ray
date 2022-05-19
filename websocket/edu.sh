463f8b69c3ff2f703584b062b4f474d41d5a66f0
Skuy Xans <73455130+natxanss@users.noreply.github.com>
2022-05-19 13:52:47 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -22,7 +22,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/bin/ws-nontls 80
+ExecStart=/usr/bin/python -O /usr/bin/ws-nontls 8880
 Restart=on-failure
 [Install]
 WantedBy=multi-user.target
